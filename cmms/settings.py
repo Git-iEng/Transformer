2286bfd90efd3d427332f7e7b00891674f489924
Git-iEng <git@ieng.tech>
2026-01-20 10:55:25 +0530

    updated both forms

--- cmms/settings.py
+++ cmms/settings.py
@@ -159,10 +159,12 @@ DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
 CONTACT_RECIPIENTS = [
     "priya@iengaust.com.au",
     "amar@iengaust.com.au",
+    "kushankur@iengaust.com.au",
    "diksha@iengaust.com.au",
    "test@ieng.tech",
 ]
-CONTACT_RECIPIENTS = [  "priya@iengaust.com.au","amar@iengaust.com.au", "diksha@iengaust.com.au"]
+CONTACT_RECIPIENTS = [  
+"test@ieng.tech","priya@iengaust.com.au","kushankur@iengaust.com.au","amar@iengaust.com.au","tushar@iengaust.com.au", "diksha@iengaust.com.au"]
 DEMO_RECIPIENTS = CONTACT_RECIPIENTS
 
 # CONTACT_EMAIL = 'diksha@iengaust.com.au'
